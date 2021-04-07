07c7e91efe93e25c2b0d86d9d9f7de1621162d6c
Max <maxbogat717@gmail.com>
2021-04-07 02:15:47 -0700

    Added labels and cleaned code

--- chorusx/src/index.js
+++ chorusx/src/index.js
@@ -45,8 +45,6 @@ var list = ['20580',
   '42740',
   ]
 
-
-
 setInterval(async function getSatalites() {
 
   const response = await fetch('http://localhost:5000/satellites');
@@ -66,17 +64,15 @@ setInterval(async function getSatalites() {
     color: 'white',
     arcAltitude: '0',
     arcStroke: '0.8',
-    satName: "<div style='padding:20px;width:500px; position: absolute; margin-top: -400px; margin-left: -500px; background-color:rgba(0,0,0,0.5); border-radius:25px;'><img  style='min-width:300px; width: 300px; text-align:center;' src=" + data[list[i]]['pictureUrl'] + "/>" + "<h1 style = 'color:white; text-align:center;'>" + data[list[i]]['satName'] + "</h1>" + "<b>" + "<p style='margin: 10px 0; padding 5px'>" + data[list[i]]['description'] + "</p><p>Launch Date: " + data[list[i]]['launchDate'] + "</p>" + "<b>" + "<br>Latitude: " +  data[list[i]]['latitude'][data[list[i]]['latitude'].length - 1] + "<br>Longitude: " + data[list[i]]['longitude'][data[list[i]]['longitude'].length - 1] + "</div>",
   }));
 
-  const arcsData2 = [...Array(13).keys()].map((i) => ({
-    endLat: data[list[i]]['latitude'][data[list[i]]['latitude'].length - 1],
-    endLng: data[list[i]]['longitude'][data[list[i]]['longitude'].length - 1],
-    pointAltitude: '0.005',
-    pointRadius:'1',
-    pointColor:'white',
-    altitude: 3.5,
-    satName: "<div style='padding:20px;width:500px; position: absolute; margin-top: -400px; margin-left: -500px; background-color:rgba(0,0,0,0.5); border-radius:25px;'><img  style='min-width:300px; width: 300px; text-align:center;' src=" + data[list[i]]['pictureUrl'] + "/>" + "<h1 style = 'color:white; text-align:center;'>" + data[list[i]]['satName'] + "</h1>" + "<b>" + "<p style='margin: 10px 0; padding 5px'>" + data[list[i]]['description'] + "</p><p>Launch Date: " + data[list[i]]['launchDate'] + "</p>" + "<b>" + "<br>Latitude: " +  data[list[i]]['latitude'][data[list[i]]['latitude'].length - 1] + "<br>Longitude: " + data[list[i]]['longitude'][data[list[i]]['longitude'].length - 1] + "</div>",
+  const labelsData = [...Array(13).keys()].map((i) => ({
+    lat: data[list[i]]['latitude'][data[list[i]]['latitude'].length - 1],
+    lng: data[list[i]]['longitude'][data[list[i]]['longitude'].length - 1],
+    labelSize: 1.0,
+    labelDotRadius: 1.5,
+    labelLabel: "<div style='padding:20px;width:700px; position: absolute; margin-top: -400px; margin-left: -500px; background-color:rgba(0,0,0,0.5); border-radius:25px;'><img  style='min-width:300px; width: 300px; text-align:center;' src=" + data[list[i]]['pictureUrl'] + "/>" + "<h1 style = 'color:white; text-align:center;'>" + data[list[i]]['satName'] + "</h1>" + "<b>" + "<p style='margin: 10px 0; padding 5px'>" + data[list[i]]['description'] + "</p><p>Launch Date: " + data[list[i]]['launchDate'] + "</p>" + "<b>" + "<br>Latitude: " +  data[list[i]]['latitude'][data[list[i]]['latitude'].length - 1] + "<br>Longitude: " + data[list[i]]['longitude'][data[list[i]]['longitude'].length - 1] + "</div>",
+    labelText: data[list[i]]['satName'],
   }));
 
   //dayMode = "//unpkg.com/three-globe/example/img/earth-day.jpg"
@@ -92,25 +88,25 @@ setInterval(async function getSatalites() {
       globeImageUrl = "//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
       bumpImageUrl = "//unpkg.com/three-globe/example/img/earth-topology.png"
       arcsData = {arcsData}
-      pointsData = {arcsData2}
-      pointLat = {'endLat'}
-      pointLng = {'endLng'}
-      pointAltitude = {'pointAltitude'}
-      pointRadius = {'pointRadius'}
-      pointColor = {'pointColor'}
-      pointLabel = {'satName'}
-      //onPointClick={d => {window.open(d.url, '_blank');console.log("CLICKED")}}
+      labelsData = {labelsData}
+      labelLat = {'lat'}
+      labelLng = {'lng'}
+      labelSize = {'labelSize'}
+      labelDotRadius = {'labelDotRadius'}
+      labelLabel = {'labelLabel'}
+      labelText = {'labelText'}
+      labelColor = {() => 'white'}
+      labelAltitude = {()=> 0.005}
       arcColor = {'color'}
       arcAltitude = {'arcAltitude'}
       arcStroke = {'arcStroke'}
       arcColor = {[`rgba(255, 255, 255, 0)`, `rgba(255, 255, 255, 0.7)`]}
-      arcLabel = {'satName'}
     />,
     
     </React.StrictMode>,
     document.getElementById('root')
   );
-}, 1500)
+}, 60000)
 
 
 
